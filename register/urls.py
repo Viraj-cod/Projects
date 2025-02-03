71c5e023d6a83e1ea206a66b5ca30d6396cda3d1
Viraj-cod <dviraj812@gmail.com>
2025-02-03 21:45:11 +0530

    removed folder

--- register/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/',views.registration,name='reg'),
-    path('',views.loginview,name='log'),
-    path('main/',views.main,name = 'main'),
-    path('logout/',views.log_out,name='logout')
-]
