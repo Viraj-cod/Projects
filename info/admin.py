0893d653d25310f070b5d1e9f051df6a783c58d9
Viraj-cod <dviraj812@gmail.com>
2025-02-03 21:44:44 +0530

    removed folder

--- info/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . models import transactions_model,Trainers_model,accesories_model,members_model
-
-admin.site.register(members_model)
-admin.site.register(transactions_model)
-admin.site.register(Trainers_model)
-admin.site.register(accesories_model)
